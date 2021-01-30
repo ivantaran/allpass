1a2d71556784bd1e04114346187969d6daa451a0
ri <ri@scaegroup.com>
2021-01-30 22:28:22 +0300

    .

--- allpass.v
+++ allpass.v
@@ -1,44 +1,57 @@
 
 module allpass #(
     parameter WIDTH = 16, 
-    parameter N = 2
+    parameter N = 5
 )(
     input wire clk, 
     input wire rst, 
     input wire signed [WIDTH-1:0] din, 
-    input wire signed [WIDTH-1:0] c, 
+    input wire signed [WIDTH*N-1:0] c, 
     output wire signed [WIDTH-1:0] dout
 );
 
-    wire signed [WIDTH-1:0] din_s[0:N-1];
-    wire signed [WIDTH-1:0] dout_s[0:N-1];
-    wire signed [WIDTH-1:0] ain_s[0:N-1];
-    wire signed [WIDTH-1:0] aout_s[0:N-1];
+    integer i;
 
-    // assign din_s[1] = aout_s[0];
-    assign ain_s[0] = din;//dout_s[1];
+    wire signed [WIDTH-1:0] cc[0:N-1];
+    reg signed [WIDTH-1:0] az[0:N-2];
+    reg signed [WIDTH-1:0] bz[0:N-2];
+    reg signed [WIDTH*2-1:0] sum;
 
-    assign din_s[0] = din;
-    assign dout = dout_s[0];
-    assign ain_s[N-1] = aout_s[N-1];
+    always @(*) begin
+        sum = din * cc[0];
+        for (i = 0; i < N - 1; i = i + 1) begin
+            sum = sum + bz[i] * cc[i+1] - az[i] * cc[N-1-i];
+        end
+    end
 
-    genvar i;
+    always @(posedge clk) begin
+        if (rst) begin
+            bz[0] <= 0;
+            az[0] <= 0;
+        end else begin
+            bz[0] <= din;
+            az[0] <= sum[WIDTH*2-1:WIDTH];
+        end
+    end
+
+    genvar g;
     generate
-        // for (i = 0; i < N - 1; i = i + 1) begin
-        //     assign din_s[i + 1] = aout_s[i];
-        //     assign ain_s[i] = dout_s[i + 1];
-        // end
-        for (i = 0; i < N; i = i + 1) begin: section
-            allpass_section u (
-                .clk(clk), 
-                .rst(rst), 
-                .c(c), 
-                .ain(ain_s[i]), 
-                .din(din_s[i]), 
-                .aout(aout_s[i]),
-                .dout(dout_s[i])
-            );
+        for (g = 0; g < N; g = g + 1) begin
+            assign cc[g] = c[WIDTH*(g+1)-1:WIDTH*g];
+        end
+        for (g = 0; g < N - 2; g = g + 1) begin
+            always @(posedge clk) begin
+                if (rst) begin
+                    bz[g + 1] <= 0;
+                    az[g + 1] <= 0;
+                end else begin
+                    bz[g + 1] <= bz[g];
+                    az[g + 1] <= az[g];
+                end
+            end
         end
     endgenerate
+
+    assign dout = az[0];
 
 endmodule
--- allpass_sim.cpp
+++ allpass_sim.cpp
@@ -4,7 +4,21 @@
 #include "verilated_vcd_c.h"
 
 vluint64_t main_time = 0;
-#define C0 32000
+
+#define SCALE 2.0
+
+#define C0 int16_t(32767 / SCALE)
+#define C1 int16_t(-20480 / SCALE)
+#define C2 int16_t(29793 / SCALE)
+#define C3 int16_t(-1025 / SCALE)
+#define C4 int16_t(-2494 / SCALE)
+#define C5 int16_t(0 / SCALE)
+// #define C0 int16_t(15793 / SCALE)
+// #define C1 int16_t(-7293 / SCALE)
+// #define C2 int16_t(32767 / SCALE)
+// #define C3 int16_t(-9097 / SCALE)
+// #define C4 int16_t(17564 / SCALE)
+// #define C5 int16_t(0 / SCALE)
 
 int main(int argc, char **argv, char **env) {
     Verilated::commandArgs(argc, argv);
@@ -19,7 +33,9 @@ int main(int argc, char **argv, char **env) {
     top->clk = 1;
     top->rst = 1;
     top->din = 0;
-    top->c = C0;
+    top->c[0] = (C1 << 16) | C0;
+    top->c[1] = (C3 << 16) | C2;
+    top->c[2] = (C5 << 16) | C4;
 
     for (int i = 0; i < 4; i++) {
         top->eval();
@@ -36,15 +52,15 @@ int main(int argc, char **argv, char **env) {
         vcd->dump(main_time);
         top->clk = top->clk ? 0 : 1;
 
-        // if (i == 1) {
-        //     top->din = 0x7fff;
-        // } else {
-        //     top->din = 0;
-        // }
+        if (i == 1) {
+            top->din = 0x7fff;
+        } else {
+            top->din = 0;
+        }
 
         main_time++;
         if (main_time % 2 == 0) {
-            top->din = sinf(2.0 * M_PI * (float)(i)*0.05) * 0x7fff;
+            // top->din = sinf(2.0 * M_PI * (float)(i)*0.05) * 0x7fff;
             i++;
         }
 
