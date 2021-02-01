402e3126c0b4feef1fad88d85e17bc22a4af74c3
ri <ri@scaegroup.com>
2021-02-01 17:50:26 +0300

    workable

--- allpass.v
+++ allpass.v
@@ -1,32 +1,37 @@
 
 module allpass #(
     parameter WIDTH = 16, 
+    parameter FIXEDPOINT = 14,
     parameter N = 5
 )(
     input wire clk, 
     input wire rst, 
     input wire signed [WIDTH-1:0] din, 
-    input wire signed [WIDTH*N-1:0] c, 
+    input wire signed [WIDTH*(N-1)-1:0] c, 
     output wire signed [WIDTH-1:0] dout
 );
 
     integer i;
 
-    wire signed [WIDTH-1:0] cc[0:N-1];
+    wire signed [WIDTH-1:0] cc[0:N-2];
     reg signed [WIDTH-1:0] az[0:N-2];
     reg signed [WIDTH-1:0] bz[0:N-2];
     reg signed [WIDTH*2-1:0] sum;
     reg signed [WIDTH*2-1:0] ma[0:N-2];
     reg signed [WIDTH*2-1:0] mb[0:N-2];
+    wire signed [WIDTH*2-1:0] sum0 = sum / 2**(FIXEDPOINT-1);
+    wire _unused = &{sum0[WIDTH*2-1:WIDTH]};
 
     always @(*) begin
         sum = din * cc[0];
-        for (i = 0; i < N - 1; i = i + 1) begin
-            ma[i] = az[i] * cc[N-1-i];
+        for (i = 0; i < N - 2; i = i + 1) begin
+            ma[i] = az[i] * cc[N-2-i];
             mb[i] = bz[i] * cc[i+1];
             sum = sum + mb[i] - ma[i];
-            // sum = sum + bz[i] * cc[i+1] - az[i] * cc[N-1-i];
         end
+        ma[N-2] = az[N-2] * cc[0];
+        mb[N-2] = bz[N-2] * 2**(FIXEDPOINT-1);
+        sum = sum + mb[N-2] - ma[N-2];
     end
 
     always @(posedge clk) begin
@@ -35,13 +40,13 @@ module allpass #(
             az[0] <= 0;
         end else begin
             bz[0] <= din;
-            az[0] <= sum[WIDTH*2-1:WIDTH];
+            az[0] <= sum0[WIDTH-1:0];
         end
     end
 
     genvar g;
     generate
-        for (g = 0; g < N; g = g + 1) begin
+        for (g = 0; g < N - 1; g = g + 1) begin
             assign cc[g] = c[WIDTH*(g+1)-1:WIDTH*g];
         end
         for (g = 0; g < N - 2; g = g + 1) begin
