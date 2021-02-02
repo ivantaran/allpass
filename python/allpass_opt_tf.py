fc18c8e9589a7709ad0347adb7d3b100dcfb0c9a
ri <ri@scaegroup.com>
2021-02-02 18:05:22 +0300

    .

--- python/allpass_opt_tf.py
+++ python/allpass_opt_tf.py
@@ -16,42 +16,49 @@ def init(n=3):
     dref = np.array([
         6.0, 9.0, 11.0, 15.0, 22.5, 27.5, 34.0, 38.0, 39.5, 41.0, 46.0, 57.5, 78.0, 110.0
     ])
-    # dref = np.cumsum(dref)
     dref = dref * 0.2 / 19.0 / lam * 360.0 / 180.0 * 1.0
 
     np.random.seed()
-    ones = np.ones(n)
-    ones[1::2] = -1
-    a = np.abs(np.random.randn(n)) * ones
+    a = np.random.randn(n)
     b = np.random.randn(1)
-    # a = np.array([8.54351048e+05, -9.94107171e+05,  2.49300089e+06,  -5.89246902e+06, 1.00000000e+00])
-    # b = -0.23547911
-    sys = signal.TransferFunction(a, np.flip(a), dt=dt)
     wsys = np.arange(24.0e6 % 562500.0, 50.0e6, 562500.0) * np.pi * 2.0 * dt
     p = {}
     p['ft'] = ft
     p['dt'] = dt
     p['lam'] = lam
-    return sys, b, wsys, dref, p
+    return a, b, wsys, dref, p
 
 
-def jacobian(wsys, sys: signal.TransferFunction, b, delta):
-    n = sys.num.shape[0]
+def a2sys(a, dt):
+    a: np.ndarray
+    n = a.shape[0]
+    p = np.array(a, dtype='complex')
+    for i in range(0, n, 2):
+        if a[i] < a[i + 1]:
+            delta = (a[i + 1] - a[i]) * 0.5
+            p[i] = np.complex(a[i], delta)
+            p[i + 1] = np.complex(a[i], -delta)
+    den = np.poly(p)
+    num = np.flip(den)
+    sys = signal.TransferFunction(num, den, dt=dt)
+    return sys
+
+
+def jacobian(wsys, a, b, dt, delta):
+    n = a.shape[0]
     m = wsys.shape[0]
-    jac = np.empty((n, m))
-    for i in range(n - 1):
-        num0 = np.array(sys.num)
-        num0[i] -= delta[i]
-        den0 = np.flip(num0)
-        sys0 = signal.TransferFunction(num0, den0, dt=sys.dt)
-        num1 = np.array(sys.num)
-        num1[i] += delta[i]
-        den1 = np.flip(num1)
-        sys1 = signal.TransferFunction(num1, den1, dt=sys.dt)
+    jac = np.empty((n + 1, m))
+    for i in range(n):
+        a0 = np.array(a)
+        a0[i] -= delta[i]
+        sys0 = a2sys(a0, dt)
+        a1 = np.array(a)
+        a1[i] += delta[i]
+        sys1 = a2sys(a1, dt)
         y0 = fx(wsys, sys0, b)
         y1 = fx(wsys, sys1, b)
         jac[i] = 0.5 * (y1 - y0) / delta[i]
-    jac[n - 1] = np.ones(m)
+    jac[n] = np.ones(m)
     return jac
 
 
@@ -76,29 +83,32 @@ def error(wsys, sys: signal.TransferFunction, b, dref, n=35):
     return e
 
 
-def tune_delta(wsys, sys, b):
-    epsilon0 = 1.0e-5
-    n = sys.num.shape[0]
-    m = wsys.shape[0]
-    delta = np.ones(n - 1)
-    while True:
-        delta0 = delta / (1.0 + 10.0 * np.random.random(n - 1))
-        jac = jacobian(wsys, sys, b, delta)[:-1]
-        jac0 = jacobian(wsys, sys, b, delta0)[:-1]
-        djac = np.abs(jac - jac0)
-        idx = djac > epsilon0
-        idx = np.sum(idx, axis=1) > 0.0
-        if np.any(idx):
-            delta[idx] = delta0[idx]
-        else:
-            break
-    return delta
+# def tune_delta(wsys, sys, b):
+#     epsilon0 = 1.0e-5
+#     n = sys.num.shape[0]
+#     m = wsys.shape[0]
+#     delta = np.ones(n - 1)
+#     while True:
+#         delta0 = delta / (1.0 + 10.0 * np.random.random(n - 1))
+#         jac = jacobian(wsys, sys, b, delta)[:-1]
+#         jac0 = jacobian(wsys, sys, b, delta0)[:-1]
+#         djac = np.abs(jac - jac0)
+#         idx = djac > epsilon0
+#         idx = np.sum(idx, axis=1) > 0.0
+#         if np.any(idx):
+#             delta[idx] = delta0[idx]
+#         else:
+#             break
+#     return delta
 
 
-n = 5
-sys, b, wsys, dref, p = init(n)
+n = 6
+a, b, wsys, dref, p = init(n)
+sys = a2sys(a, p['dt'])
+
 lam = 0.001
-delta = tune_delta(wsys, sys, b)
+# delta = tune_delta(wsys, sys, b)
+delta = np.ones(n) * 1.0e-9
 cc = []
 sigma = 1.0e0
 
@@ -123,22 +133,21 @@ batch_size = 1000
 i = 0
 lam0 = 1.0e-3  # 7.03181e-12
 ok = True
-delta = tune_delta(wsys, sys, b)
+# delta = tune_delta(wsys, sys, b)
 identity = np.identity(sys.num.shape[0])
 while True:
-    jac = jacobian(wsys, sys, b, delta)
+    jac = jacobian(wsys, a, b, sys.dt, delta)
     h = np.matmul(jac, jac.transpose())
     # hinv = np.linalg.pinv(h)
     # hinv = np.linalg.pinv(lam0 * np.identity(sys.num.shape[0]))
-    hinv = np.linalg.pinv(h + lam0 * identity * h + identity * 1000.0)
+    hinv = np.linalg.pinv(h + lam0 * identity * h + identity * 1.0)
     e = np.reshape(error(wsys, sys, b, dref), (wsys.shape[0], 1))
     da = np.matmul(hinv, jac)
     da = np.matmul(da, e)
-    num = np.array(sys.num)
-    num[:-1] -= np.squeeze(da[:-1]) * sigma
+    a0 = np.array(a)
+    a0 -= np.squeeze(da[:-1]) * sigma
     b0 = b - da[-1] * sigma
-    den = np.flip(num)
-    sys0 = signal.TransferFunction(num, den, dt=sys.dt)
+    sys0 = a2sys(a0, sys.dt)
     c = c0
     c0 = cost(wsys, sys0, b0, dref)
 
@@ -148,6 +157,7 @@ while True:
 
     if c0 < cmin:
         b = b0
+        a = a0
         sys = sys0
         cmin = c0
         # lam0 /= 1.0 + np.random.random() * 5.0
@@ -163,7 +173,7 @@ while True:
 
     cc.append(c0)
     if (i % batch_size) == 0:
-        delta = tune_delta(wsys, sys, b)
+        # delta = tune_delta(wsys, sys, b)
         y = fx(wsys, sys, b)
         # print(c)
         ax: pyplot.Axes
@@ -185,8 +195,8 @@ while True:
         ax1.plot(np.log10(cc) * 10.0, 'o-')
 
         ax3.scatter(np.real(sys.poles), np.imag(sys.poles))
-        # ax3.set_xlim([-1.5, 1.5])
-        # ax3.set_ylim([-1.5, 1.5])
+        ax3.set_xlim([-1.5, 1.5])
+        ax3.set_ylim([-1.5, 1.5])
         ax3.add_patch(circle)
 
         fig.canvas.draw()
